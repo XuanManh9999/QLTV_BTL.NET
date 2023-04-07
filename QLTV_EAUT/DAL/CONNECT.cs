6b130d308803900043488ca24a3e94b3204aa2f2
Mr Manh Pc <nguyenxuanmanh2992003@gmail.com>
2023-04-07 23:44:57 +0700

    them chuoi ket noi

---
+++ QLTV_EAUT/DAL/CONNECT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CONNECT
+    {
+        public SqlConnection chuoiKetNoi()
+        {
+            string strCon = @"Data Source=DESKTOP-LNJ99RH\SQLEXPRESS;Initial Catalog=QUANLYTHUVIEN;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(strCon);
+            sqlCon.Open();
+            return sqlCon;
+        }
+    }
+}
